67cd0bc73016d0cf31c868e5575fb3347fbea4c1
Robson <robsonad07@gmail.com>
2024-02-19 21:00:44 -0300

    base do servidor criado

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+  res.render("index.ejs");
+});
+
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
